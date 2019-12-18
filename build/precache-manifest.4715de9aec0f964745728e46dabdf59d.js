fadff07384f37305448a26d441bb3b9140c25991
limengyun111 <2753@qq.com>
2019-12-18 12:13:06 +0800

    add build

---
+++ build/precache-manifest.4715de9aec0f964745728e46dabdf59d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137d8ed8d9f258172978ef6aac152bf2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2dff7048a818e92be31",
+    "url": "./static/css/2.5c4f5620.chunk.css"
+  },
+  {
+    "revision": "eb6dfe0942786fc30bee",
+    "url": "./static/css/main.e2f8b536.chunk.css"
+  },
+  {
+    "revision": "b2dff7048a818e92be31",
+    "url": "./static/js/2.e9b18bf6.chunk.js"
+  },
+  {
+    "revision": "db3749dbc1774fd0419150b7da2ac7a8",
+    "url": "./static/js/2.e9b18bf6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb6dfe0942786fc30bee",
+    "url": "./static/js/main.869318d6.chunk.js"
+  },
+  {
+    "revision": "b71340a2540ecdc1883b",
+    "url": "./static/js/runtime-main.5c89be09.js"
+  }
+]);
